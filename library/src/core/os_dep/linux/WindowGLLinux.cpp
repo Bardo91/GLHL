5b4dc911f786cd30038e52851e9c43c3e0db3df2
Bardo9 <pabramsor@gmail.com>
2014-11-07 21:45:24 +0100

    Renamed windowGL to WindowGL. Implemented WindowGLLinux but not tested. Fixed makefile (Added SOIL dependencies)

---
+++ library/src/core/os_dep/linux/WindowGLLinux.cpp
@@ -0,0 +1,86 @@
+//////////////////////////////////////////////////////////////////////////
+//																		//
+//		OpenGL Helper Libraries for CPU Processing  (GLHL)				//
+//			Author: Pablo Ramón Soria									//
+//			Date:	2014-01-28											//
+//																		//
+//////////////////////////////////////////////////////////////////////////
+//
+
+#ifdef __linux__
+
+#include "WindowGLLinux.h"
+
+#include <iostream>
+#include <cassert>
+
+namespace GLHL {
+	namespace GLHL_LINUX{
+		//---------------------------------------------------------------------------------
+		WindowGLLinux::WindowGLLinux(int _width, int _height){
+			mWidth = _width;
+			mHeight = _height;
+
+			initializeX();
+
+			initializeWindow();
+		}
+		//---------------------------------------------------------------------------------
+		void WindowGLLinux::swapBuffers(){
+			
+		}
+
+		//---------------------------------------------------------------------------------
+		void WindowGLLinux::initializeX(){
+			 mDpy = XOpenDisplay(NULL);	// Null means that the graphical output will be send to this same computer.
+ 	
+ 			if(mDpy == NULL) {
+				std::cout << "cannot connect to X server" << std::endl;
+				assert(false);	// Cant connect to X server.
+			}
+
+			mRoot = DefaultRootWindow(mDpy);	// Handle to the root window.
+
+		}
+
+		//---------------------------------------------------------------------------------
+		void WindowGLLinux::initializeWindow(){
+			mVi = glXChooseVisual(mDpy, 0, mAtt);	// Select visual adapted to our needs ("mAtt").
+
+			if(mVi == NULL) {
+				std::cout << "No appropriate visual found" << std::endl;
+				assert(false);	//	No appropriate visual found.
+			} 
+			std::cout << "visual " << ((void *)mVi->visualid) << " selected" << std::endl;
+
+			// Create a colormap for the window.
+			mCmap = XCreateColormap(mDpy, mRoot, mVi->visual, AllocNone);
+
+			// Fill structure with window attributes.
+			mSwa.colormap = mCmap;
+			mSwa.event_mask = ExposureMask | KeyPressMask;
+
+			// Create a window with previous data.
+			mWin = XCreateWindow(mDpy, mRoot, 0, 0, mWidth, mHeight, 0, mVi->depth, InputOutput, mVi->visual, CWColormap | CWEventMask, &mSwa);	
+
+			// Make the window apperas
+			XMapWindow(mDpy, mWin);
+
+			// Change window's name
+			XStoreName(mDpy, mWin, mWndName.c_str());
+
+			// Create the openGL context
+			mGlc = glXCreateContext(mDpy, mVi, NULL, GL_TRUE);	// Last parameter set direct rendering direct
+ 			glXMakeCurrent(mDpy, mWin, mGlc);
+
+		}
+
+		//---------------------------------------------------------------------------------
+		bool WindowGLLinux::selfDestroy(){
+			
+			return true;
+		}
+	}	// namespace GLHL_LINUX
+}	// namespace GLHL.
+
+#endif
---
+++ library/src/core/os_dep/linux/WindowGLLinux.h
@@ -0,0 +1,61 @@
+//////////////////////////////////////////////////////////////////////////
+//																		//
+//		OpenGL Helper Libraries for CPU Processing  (GLHL)				//
+//			Author: Pablo Ramón Soria									//
+//			Date:	2014-01-28											//
+//																		//
+//////////////////////////////////////////////////////////////////////////
+//
+
+#ifdef __linux__
+
+#ifndef _GLHL_CORE_OSDEP_LINUX_WINDOWLINUX_H_
+#define _GLHL_CORE_OSDEP_LINUX_WINDOWLINUX_H_
+
+#include "../../WindowGL.h"
+
+#include <GL/glew.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <GL/glx.h>
+
+#include <string>
+
+namespace GLHL{
+	namespace GLHL_LINUX{
+		class WindowGLLinux : public WindowGL{
+		public:
+			WindowGLLinux(int _width, int _height);
+
+		public:
+			void swapBuffers();
+
+		private:
+			void initializeX();
+			void initializeWindow();
+
+			bool selfDestroy();
+
+		private:
+
+			std::string mWndName = "Test";
+
+			Display                 *mDpy;
+			Window                  mRoot;
+			GLint                   mAtt[5] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };	// Capabilities that the program needs
+			XVisualInfo             *mVi;
+			Colormap                mCmap;
+			XSetWindowAttributes    mSwa;
+			Window                  mWin;
+			GLXContext              mGlc;
+			XWindowAttributes       mGwa;
+			XEvent                  mXev;
+
+		};
+	}	//namespace GLHL_LINUX
+}	// namespace GLHL
+
+
+#endif	// _GLHL_CORE_OSDEP_LINUX_WINDOWLINUX_H_
+
+#endif
