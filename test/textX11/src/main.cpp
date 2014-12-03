6ee5d94636740a6289ebe40a7a1b8cff4297201a
Bardo9 <pabramsor@gmail.com>
2014-12-04 00:11:41 +0100

    Updated gitignore. Testing pbuffers

--- test/textX11/src/main.cpp
+++ test/textX11/src/main.cpp
@@ -5,14 +5,14 @@
 #include <iostream>
 #include <cassert>
 
-
 #include <GL/glx.h>
 #include <X11/Xlib.h>
 
 using namespace std;
 
-void queryEveryConfig(Display *_display);
-void queryConfigAttb(Display *_display, const int * _attribs);
+void showFBConfigs(GLXFBConfig *_configs, int _nElem);
+GLXFBConfig* queryEveryConfig(Display *_display);
+GLXFBConfig* queryConfigAttb(Display *_display, const int * _attribs);
 
 int main(void) {
 	Display *display = XOpenDisplay(0);
@@ -26,102 +26,92 @@ int main(void) {
 	GLX_RED_SIZE, 8, GLX_BLUE_SIZE, 8, GLX_RED_SIZE, 8,
 	GLX_DRAWABLE_TYPE, GLX_PBUFFER, None };
 
-	queryConfigAttb(display, attribsPbuffer);
+	GLXFBConfig *configsPbuffer = queryConfigAttb(display, attribsPbuffer);
 
-return 0;
+	// Creating Pbuffer
+	GLXPbuffer pbuffer = glXCreatePbuffer(display, configsPbuffer[1], nullptr);
+
+	GLXContext contextpbuffer = glXCreateNewContext(display, configsPbuffer[1],
+			GLX_RGBA_TYPE, NULL, true);
+
+	if (glXMakeContextCurrent(display, pbuffer, pbuffer, contextpbuffer)) {
+		std::cout << "Current context is a pbuffer" << std::endl;
+
+	}
+
+	// Creating Pixmap
+
+	// Creating Window
+
+	return 0;
 }
 
 //---------------------------------------------------------------------------
-void queryEveryConfig(Display *_display) {
+void showFBConfigs(Display *_display, GLXFBConfig *_configs, int _nElem) {
+	std::cout
+			<< "ID\t bufSize\t dble\t auxBuff\t rSize\t bSize\t gSize\t aSize\t dSize\t drawType\t visualId "
+			<< std::endl;
+	for (int i = 0; i < _nElem; i++) {
+		int value = 0;
+
+		int succes = -1;
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_FBCONFIG_ID,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_BUFFER_SIZE,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_DOUBLEBUFFER,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_AUX_BUFFERS,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_RED_SIZE,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_BLUE_SIZE,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_GREEN_SIZE,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_ALPHA_SIZE,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_DEPTH_SIZE,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_DRAWABLE_TYPE,
+				&value);
+		std::cout << value << "\t";
+		succes = glXGetFBConfigAttrib(_display, _configs[i], GLX_VISUAL_ID,
+				&value);
+		std::cout << value << "\n";
+
+		assert(succes == 0);
+	}
+
+}
+
+//---------------------------------------------------------------------------
+GLXFBConfig* queryEveryConfig(Display *_display) {
 	int screen = 0;
 	int nElem;
 	GLXFBConfig *configs = glXGetFBConfigs(_display, screen, &nElem);
 
-	std::cout << "Configuration available on this screen: " << std::endl;
-
-	std::cout
-			<< "ID\t bufSize\t dble\t auxBuff\t rSize\t bSize\t gSize\t aSize\t dSize\t drawType\t visualId "
-			<< std::endl;
-	for (int i = 0; i < nElem; i++) {
-		int value = 0;
-
-		int succes = -1;
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_FBCONFIG_ID,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_BUFFER_SIZE,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_DOUBLEBUFFER,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_AUX_BUFFERS,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_RED_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_BLUE_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_GREEN_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_ALPHA_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_DEPTH_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_DRAWABLE_TYPE,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_VISUAL_ID, &value);
-		std::cout << value << "\n";
-
-		assert(succes == 0);
-	}
+	showFBConfigs(_display, configs, nElem);
+	return configs;
 }
 
 //---------------------------------------------------------------------------
-void queryConfigAttb(Display *_display, const int * _attribs) {
+GLXFBConfig* queryConfigAttb(Display *_display, const int * _attribs) {
 	int screen = 0;
 	int nElem;
-	GLXFBConfig *configs = glXChooseFBConfig(_display, screen, _attribs,  &nElem);
+	GLXFBConfig *configs = glXChooseFBConfig(_display, screen, _attribs, &nElem);
 
-	std::cout << "Configuration available on this screen: " << std::endl;
+	showFBConfigs(_display, configs, nElem);
 
-	std::cout
-			<< "ID\t bufSize\t dble\t auxBuff\t rSize\t bSize\t gSize\t aSize\t dSize\t drawType\t visualId "
-			<< std::endl;
-	for (int i = 0; i < nElem; i++) {
-		int value = 0;
-
-		int succes = -1;
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_FBCONFIG_ID,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_BUFFER_SIZE,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_DOUBLEBUFFER,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_AUX_BUFFERS,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_RED_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_BLUE_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_GREEN_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_ALPHA_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_DEPTH_SIZE, &value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_DRAWABLE_TYPE,
-				&value);
-		std::cout << value << "\t";
-		succes = glXGetFBConfigAttrib(_display, configs[i], GLX_VISUAL_ID, &value);
-		std::cout << value << "\n";
-
-		assert(succes == 0);
-	}
+	return configs;
 }
 //---------------------------------------------------------------------------
