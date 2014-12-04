0f92135725d9e25ce795366356e410ee23c5ba2d
Bardo9 <pabramsor@gmail.com>
2014-12-04 20:30:29 +0100

    Finished test with x11

--- test/textX11/src/main.cpp
+++ test/textX11/src/main.cpp
@@ -6,6 +6,7 @@
 #include <cassert>
 
 #include <GL/glx.h>
+#include <X11/X.h>
 #include <X11/Xlib.h>
 
 using namespace std;
@@ -22,22 +23,22 @@ int main(void) {
 
 	//queryEveryConfig(display);
 
-	int attribsPbuffer[] = { GLX_DOUBLEBUFFER, 0, GLX_BUFFER_SIZE, 32,
-	GLX_RED_SIZE, 8, GLX_BLUE_SIZE, 8, GLX_RED_SIZE, 8,
-	GLX_DRAWABLE_TYPE, GLX_PBUFFER, None };
+	//int attribsPbuffer[] = { GLX_DOUBLEBUFFER, 0, GLX_BUFFER_SIZE, 32,
+	//GLX_RED_SIZE, 8, GLX_BLUE_SIZE, 8, GLX_RED_SIZE, 8,
+	//GLX_DRAWABLE_TYPE, GLX_PBUFFER, None };
 
-	GLXFBConfig *configs = queryConfigAttb(display, attribsPbuffer);
+	//GLXFBConfig *configs = queryConfigAttb(display, attribsPbuffer);
 
 	// Creating Pbuffer
-	GLXPbuffer pbuffer = glXCreatePbuffer(display, configs[1], nullptr);
-
-	GLXContext contextpbuffer = glXCreateNewContext(display, configs[1],
-			GLX_RGBA_TYPE, NULL, true);
-
-	if (glXMakeContextCurrent(display, pbuffer, pbuffer, contextpbuffer)) {
-		std::cout << "Current context is a pbuffer" << std::endl;
-
-	}
+	//GLXPbuffer pbuffer = glXCreatePbuffer(display, configs[1], nullptr);
+    //
+	//GLXContext contextpbuffer = glXCreateNewContext(display, configs[1],
+	//		GLX_RGBA_TYPE, NULL, true);
+    //
+	//if (glXMakeContextCurrent(display, pbuffer, pbuffer, contextpbuffer)) {
+	//	std::cout << "Current context is a pbuffer" << std::endl;
+    //
+	//}
 
 	// Creating Pixmap
 	//XVisualInfo * visualInfo = glXChooseVisual(display, 0, attribsPbuffer);
@@ -45,27 +46,51 @@ int main(void) {
 	//GLXPixmap pixmap = glXCreatePixmap(display, configs[1], pixmap, nullptr);
 
 	// Creating Window
-	int attribsWindow[] = { GLX_DOUBLEBUFFER, 1, GLX_BUFFER_SIZE, 32,
-	GLX_RED_SIZE, 8, GLX_BLUE_SIZE, 8, GLX_RED_SIZE, 8,
-	GLX_DRAWABLE_TYPE, GLX_WINDOW, None };
+	//int attribsWindow[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };	// Capabilities that the program needs
+    //
+	//XVisualInfo *visual = glXChooseVisual(display, 0, attribsWindow);
+	//Window rootWnd = DefaultRootWindow(display);
+    //
+	//XSetWindowAttributes wndAttrib;
+	//wndAttrib.colormap = XCreateColormap(display, rootWnd, visual->visual, AllocNone);
+	//wndAttrib.event_mask = ExposureMask | KeyPressMask;
+    //
+	//Window window = XCreateWindow(	display,		// X display
+	//								rootWnd, 		// parent window
+	//								0, 0, 640,480,	// X, Y, width, and height,
+	//								0,				// Border width
+	//								visual->depth,  //32,				//depth
+	//								InputOutput,		// type
+	//								visual->visual,
+	//								CWColormap| CWEventMask,
+	//								&wndAttrib);
+    //
+    //
+	////XStoreName(display, window, "testing X11");
+	//XMapWindow(display, window);
+	//GLXContext context = glXCreateContext(display, visual, NULL, true);
+	//glXMakeCurrent(display, window, context);
 
+	const int attribList[] = {	GLX_DOUBLEBUFFER,
+								GLX_RED_SIZE, 8,
+								GLX_GREEN_SIZE, 8,
+								GLX_BLUE_SIZE, 8 };
+	int nElem;
+	int screen = 0;
 
-	XVisualInfo visual = glXChooseVisual(display, 0, attribsWindow);
-	Window rootWnd = XRootWindow(display, 0);
-	Window window = XCreateWindow(	display,		// X display
-									rootWnd, 		// parent window
-									0, 0, 640,480,	// X, Y, width, and height,
-									0,				// Border width
-									32,				//depth
-									InputOutput,		// type
-									visual.visual,
-									0,
-									attrib);
+	GLXFBConfig *config = glXChooseFBConfig(display, screen, attribList, &nElem);
 
+	Window Xwindow = XCreateWindow(display, DefaultRootWindow(display), )
 
-	GLXWindow = glXCreateWindow(display, configWnd, window, attribsWindow);
+	GLXWindow window = glXCreateWindow(display, config[0], win, attribList);
 
 	// END
+	bool condition = true;
+
+	while(condition){
+
+	}
+
 	return 0;
 }
 
