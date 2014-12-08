4350d819ddb3aa30e8b099406ed0e9ec5eafcc40
Bardo91 <19Bardo91@gmail.com>
2014-12-08 18:15:09 +0100

    Added time class

---
+++ library/src/core/utils/time/time.h
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//	BOVIL - core
+//
+//		Author: Pablo Ram�n Soria 
+//		Date: 2013/04/08
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Time and time functions
+
+#ifndef _GLHL_CORE_UTILS_TIME_TIME_H_
+#define _GLHL_CORE_UTILS_TIME_TIME_H_
+
+#if defined(__linux__)
+	#include <sys/time.h>
+#elif defined(_WIN32)
+	#include <Windows.h>
+#endif
+
+#include <iostream>
+
+namespace BOViL {
+	class STime {
+	public:
+		// --- Singleton life cycle ---
+		static void init();
+		static STime* get();        // Returns the singleton instance
+		static void end();
+
+	public: // --- Public interface ---
+		double getTime();
+
+	private:
+		STime();
+	private:
+		// Singleton instance
+		static STime* sTime; // Static data definition
+		// members
+		#if defined(__linux__)
+			timeval mInitTime;
+		#elif defined (_WIN32)
+			LARGE_INTEGER mInitTime;
+		#endif
+	};
+
+	//------------------------------------------------------------------------------------------------------------------
+	inline STime * STime::get() {
+		return sTime;
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	inline double STime::getTime() {
+	#if defined (__linux__)
+			// Get current time
+			timeval currentTime;
+			gettimeofday(&currentTime, 0);
+			return double(currentTime.tv_sec - mInitTime.tv_sec) + double(currentTime.tv_usec - mInitTime.tv_usec) / 1000000;
+	#elif defined (_WIN32)
+			// Get current time
+			LARGE_INTEGER largeTicks;
+			QueryPerformanceCounter(&largeTicks);
+			unsigned currTime = largeTicks.LowPart;
+			// Convert time difference to seconds
+			LARGE_INTEGER frequency;
+			QueryPerformanceFrequency(&frequency);
+			return (double(currTime) / double(frequency.LowPart)) -
+				(double(mInitTime.LowPart) / double(frequency.LowPart));
+	#endif 
+	}
+
+}        // namespace BOViL
+
+#endif // _GLHL_CORE_UTILS_TIME_TIME_H_
