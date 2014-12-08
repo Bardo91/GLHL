0f67c49f791afe2e978035a41bc7ce71b0c69a1d
Bardo91 <19Bardo91@gmail.com>
2014-12-08 19:16:11 +0100

    First approx of sobel performance test. sobel process is 20 times faster in GPU than in CPU. But GPU needs the image to be uploaded. ¿Why so slow? ¿May be SOIL trouble? Design performance improvements with double threading etc...

--- test/sobelPerformance/src/main.cpp
+++ test/sobelPerformance/src/main.cpp
@@ -24,12 +24,19 @@ void drawQuad();
 
 int main(void){
 
+	STime::init();
 
+	std::cout << "Image characteristics: " << std::endl;
+	std::cout << "\t Width = 1024" << std::endl;
+	std::cout << "\t Height = 768" << std::endl;
+	std::cout << "\t Channels = 1" << std::endl;
 
+	
 	sobelCPU();
-
+	
 	sobelGLHL();
 
+	
 	#ifdef _WIN32
 	system("PAUSE");
 	#endif
@@ -37,22 +44,79 @@ int main(void){
 	return 0;
 }
 
-void sobelCPU(){
+void sobelCPU(){		// Dont see >.<. Embarrassing code
 	int width;
 	int height;
 	int channels;
 
-	unsigned char *image = SOIL_load_image("./Tulips.jpg", &width, &height, &channels, SOIL_LOAD_AUTO);
+	STime *time = STime::get();
+
+	double t1 = time->getTime();
+	unsigned char *image = SOIL_load_image("./Tulips.jpg", &width, &height, &channels, 1);
+	double t2 = time->getTime();
+	
+
+	
+	char sobelH[] = { 1.0, 2.0, 1.0, 0.0, 0.0, 0.0, -1.0, -2.0, -1.0 };
+	char sobelV[] = { -1.0, 0.0, 1.0, -2.0, 0.0, 2.0, -1.0, 0.0, 1.0 };
 
 
+	for (int i = 1; i < width - 1; i++){
+		for (int j = 1; j < height - 1; j++){
+			char c1 = image[(i - 1)*height + j - 1];
+			char c2 = image[(i - 1)*height + j];
+			char c3 = image[(i - 1)*height + j + 1];
+			char c4 = image[(i)*height + j - 1];
+			char c5 = image[(i)*height + j];
+			char c6 = image[(i)*height + j + 1];
+			char c7 = image[(i + 1)*height + j - 1];
+			char c8 = image[(i + 1)*height + j];
+			char c9 = image[(i + 1)*height + j + 1];
+
+			int Gx =	sobelH[0] * c1 +
+						sobelH[1] * c2 + 
+						sobelH[2] * c3 + 
+						sobelH[3] * c4 + 
+						sobelH[4] * c5 + 
+						sobelH[5] * c6 +
+						sobelH[6] * c7 +
+						sobelH[7] * c8 + 
+						sobelH[8] * c9 ;
+
+			int Gy =	sobelV[0] * c1 +
+						sobelV[1] * c2 +
+						sobelV[2] * c3 +
+						sobelV[3] * c4 +
+						sobelV[4] * c5 +
+						sobelV[5] * c6 +
+						sobelV[6] * c7 +
+						sobelV[7] * c8 +
+						sobelV[8] * c9;
+
+
+
+			double val = sqrt(Gx*Gx + Gy*Gy);
+
+			if (val > 0.3 * 255)
+				image[i*height + j] = 1;
+			else
+				image[i*height + j] = 0;
+
+
+		}
+	}
+
+	double t3 = time->getTime();
+	std::cout << "SOIL's image load spent: " << t2 - t1 << std::endl;
+	std::cout << "CPU spent: " << t3 - t2 << std::endl;
 }
 
 void sobelGLHL(){
 	WindowGL window("Sobel Performance", 1024, 768);
-
+	//window.hide();
 	DriverGPU * driver = DriverGPU::get();
 
-	Texture texture("./Tulips.jpg");
+	
 
 	Shader vShader(eShaderType::eVertexShader, "../../src/shaders/flat.vertex");
 	Shader fShader(eShaderType::eFragmentShader, "../../src/shaders/sobel.fragment");
@@ -63,11 +127,22 @@ void sobelGLHL(){
 	program.attachShader(fShader);
 	program.link();
 
-	window.peekMessage();
-	drawImage(texture, program);
+	STime *time = STime::get();
+	double t1 = time->getTime();
+	Texture texture("./Tulips.jpg");
 
-	texture.saveTexture("result.bmp");
+	
+	double t2 = time->getTime();
+	drawImage(texture, program);
+		
+	double t3 = time->getTime();
+
+	std::cout << "SOIL's image load and upload to GPU spent: " << t2 - t1 << std::endl;
+	std::cout << "GPU spent: " << t3 - t2 << std::endl;
+
 	window.swapBuffers();
+	texture.saveTexture("result.bmp");
+
 
 	delete driver;
 
