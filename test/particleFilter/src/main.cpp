e5b082344c0423d039208124bc1adc66b74cb1b5
Bardo91 <19Bardo91@gmail.com>
2014-12-21 08:43:52 +0100

    Created project to test particle filtering with GLHL

---
+++ test/particleFilter/src/main.cpp
@@ -0,0 +1,70 @@
+//// MAIN FILE TO TEST
+
+
+#include <src/core/base/WindowGL.h>
+#include <src/core/base/DriverGPU.h>
+#include <src/core/glsl/Shader.h>
+#include <src/core/glsl/ShaderProgram.h>
+
+using namespace GLHL;
+
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+GLvoid drawOnBuffer(GLint _width, GLint _height, ShaderProgram _program);
+
+int main(void){
+	WindowGL window("Triangle test", 640, 480);
+
+	DriverGPU *driver = DriverGPU::get();
+
+	Shader vShader(eShaderType::eVertexShader, "../../src/shaders/particleFilter.vertex");
+	Shader fShader(eShaderType::eFragmentShader, "../../src/shaders/quad.fragment");
+
+	ShaderProgram sProgram;
+	sProgram.attachShader(vShader);
+	sProgram.attachShader(fShader);
+	sProgram.bindAttribute(0, "vPosition");
+	//driver->bindFragDataLocation(sProgram, 0, "outColor");
+	sProgram.link();
+
+
+	while(1){
+	#ifdef _WIN32
+		window.peekMessage();
+	#endif
+
+		drawOnBuffer(640, 480, sProgram);
+
+		window.swapBuffers();
+	}
+
+	//	delete window; 	Class has no destructor, undefined behaviour
+	delete driver;
+
+	return 0;
+}
+
+//---------------------------------------------------------------------------
+
+GLvoid drawOnBuffer(GLint _width, GLint _height, ShaderProgram _program){
+	GLfloat vVertices[] = { 0.0f, 0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f };
+	glViewport(0, 0, _width, _height);
+
+	//glClear(GL_COLOR_BUFFER_BIT);
+
+	DriverGPU* driver = DriverGPU::get();
+
+	_program.use();
+
+	// Load vertex Data
+	driver->vertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
+	driver->enableVertexAttribArray(0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
