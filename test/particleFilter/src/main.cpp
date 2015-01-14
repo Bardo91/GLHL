756032095668aeae855095bf4b848fbefab49f45
Bardo91 <19Bardo91@gmail.com>
2015-01-15 00:25:15 +0100

    calculated step time for performance analysis

--- test/particleFilter/src/main.cpp
+++ test/particleFilter/src/main.cpp
@@ -13,6 +13,7 @@
 #include <ctime>
 	
 #include <src/core/base/Context.h>
+#include <src/core/utils/time/time.h>
 
 #include "ParticleFilterGPU.h"
 #include "ParticleFilterCPU.h"
@@ -39,9 +40,9 @@ void particleFilterGPU();
 //---------------------------------------------------------------------------------------------------------------------
 int main(void){
 	
-	particleFilterCPU();
+	//particleFilterCPU();
 	
-	//particleFilterGPU();
+	particleFilterGPU();
 
 	std::cout << "Finished" << std::endl;
 	
@@ -69,14 +70,17 @@ std::array<double, 3> mediumState(std::vector<ParticleRobot> _particles){
 void particleFilterCPU() {
 	srand(unsigned(time(NULL)));
 
-	ParticleFilterCPU<ParticleRobot> filter(1000);
+	ParticleFilterCPU<ParticleRobot> filter(10000);
 	filter.init();
 
 	ParticleRobot robot;
-
-	for (unsigned i = 0; i < 10; i++) {
+	STime *sTime = STime::get();
+	double time = 0.0;
+	for (unsigned i = 0; i < 50; i++) {
+		double t0 = sTime->getTime();
 		robot.simulate();
 		filter.step(robot);
+		time += sTime->getTime() - t0;
 
 		std::array<double, 3> medState = mediumState(filter.particles());
 		std::array<double, 3> realState = robot.position();
@@ -84,6 +88,8 @@ void particleFilterCPU() {
 		std::cout << "Real state. X:" << realState[0] << " ; Y: " << realState[1] << " ; Ori: " << realState[2] << std::endl;
 		std::cout << "Promediate state. X:" << medState[0] << " ; Y: " << medState[1] << " ; Ori: " << medState[2] << std::endl;
 	}
+	std::cout << "Step Time: " << time / 50 << std::endl;
+
 }
 
 void particleFilterGPU() {
@@ -95,13 +101,19 @@ void particleFilterGPU() {
 	robot.setNoise(0.05, 0.05, 5.0);
 
 	// 666 TODO Relative path to vertex shader?
-	ParticleFilterGPU pfGPU(10000, "../../src/shaders/particleFilterShaderTemplate.fragment");
-
-	for (unsigned i = 0; i < 10; i++) {
+	ParticleFilterGPU pfGPU(1000, "../../src/shaders/particleFilterShaderTemplate.fragment");
+	STime *sTime = STime::get();
+	double time = 0.0;
+	for (unsigned i = 0; i < 50; i++) {
+		double t0 = sTime->getTime();
 		robot.move(0.1, 5.0);
 		std::array<double, 4> measure = robot.sense();
 		std::cout << "Real particle: " << robot << std::endl;
 		pfGPU.step(vec4f(float(measure[0]), float(measure[1]), float(measure[2]), float(measure[3])));
 		std::cout << "--------------------------------------------------------------------------------------" << std::endl;
+
+		time += sTime->getTime() - t0;
 	}
+
+	std::cout << "Step Time: " << time / 50 << std::endl;
 }
